99862be9d6c13f4a863ed006490f9cf39e8f8efa
Abdulrahman Aminu <aminuabdulrahmandayo@gmail.com>
2022-11-11 19:39:17 +0100

    1. Test in servo class api if called run as a module 2. inclusion into robot class

--- lib/servos.py
+++ lib/servos.py
@@ -14,12 +14,46 @@ class Servos:
         else:
             print("[Error]: Servo intialization failed due to wrong pin selection")
 
+        # Start Up
+        self.set_angle(self.start_position)
+
     def set_angle(self, angle: int):
-        if self.initState and (self.min_angle <= angle and self.max_angle >= angle):
+        if self.initState and self.verify(angle):
             self._motor_hat.setOutput(self._output_pin, angle)
         else:
             print("[Error]: Angle outside allowed region")
 
     def cleanup(self):
-        if self.initState and (self.min_angle <= self.start_position and self.max_angle >= self.start_position):
+        if self.initState and self.verify(self.start_position):
             self._motor_hat.setOutput(self._output_pin, self.start_position)
+
+    def verify(self, angle: int):
+        if (self.min_angle <= angle and self.max_angle >= angle):
+            return True
+        return False
+
+
+# Testing Pan and Tilt Servos
+if __name__ == "__main__":
+    import time
+    import piconzero as pz
+    pz.init()
+
+    pan_pin = 1
+    pan_swing_angle = (50, 150)
+    pan_start_angle = 103
+
+    tilt_pin = 0
+    tilt_swing_angle = (55, 180)
+    tilt_start_angle = 155
+
+    pan = Servos(motor_hat=pz, output_pin=pan_pin,
+                 min_max_degree=pan_swing_angle, start_position=pan_start_angle)
+    tilt = Servos(motor_hat=pz, output_pin=tilt_pin,
+                  min_max_degree=tilt_swing_angle, start_position=tilt_start_angle)
+
+    pan.set_angle(65)
+    tilt.set_angle(170)
+    time.sleep(2)
+    pan.cleanup()
+    tilt.cleanup()
